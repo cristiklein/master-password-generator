0cca1beddaae902c49319b2158b29908940cdf64
Cristian KLEIN <cristiklein@gmail.com>
2012-04-19 23:19:21 +0200

    First public commit

---
+++ masterPasswordMaker.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import argparse
+import math
+import random
+
+# Parse command line
+parser = argparse.ArgumentParser(description = 'Generate a random password from a wordlist')
+parser.add_argument('--file'    , default = 'words.txt', help='file to get wordlist from (default: words.txt)')
+parser.add_argument('--numWords', type = int, default = 5, help='number of words to generate (default: 5)')
+args = parser.parse_args()
+
+# Read wordlist
+words = []
+with open(args.file) as f:
+	for line in f.readlines():
+		words.append(line.strip())
+
+# Generate password
+passwordLength = 0
+for _ in range(0, args.numWords):
+	word = random.choice(words)
+	passwordLength += len(word)
+	print(word)
+
+# Show statistics
+estimatedEntropy = args.numWords * math.log(len(words), 2)
+print("Entropy: {0}".format(estimatedEntropy))
+print("Length : {0}".format(passwordLength))
+
